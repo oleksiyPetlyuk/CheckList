4e4c19e455ac126cfbadb27d991a2b5bab66463e
Oleksiy Petlyuk <oleksiy.petlyuk94@gmail.com>
2019-11-24 12:00:35 +0200

    Initial Commit

---
+++ CheckList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Oleksiy Petlyuk on 24.11.2019.
+//  Copyright © 2019 Oleksiy Petlyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
